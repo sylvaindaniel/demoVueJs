7fe7c14631fb47d300a5465b20540c9deb966138
Sylvain Daniel <sylvain.daniel@infotel.com>
2023-10-27 15:39:22 +0200

    message

---
+++ src/components/NavLink.vue
@@ -0,0 +1,24 @@
+// ./components/NavLink.vue
+<script>
+export default {
+  // Nom du composant (utilisé pour le débogage, n'affecte pas le comportement).
+  //doit etre en multi-word (NavLink ou nav-link mais pas navlink)
+  name: 'NavLink',
+
+  // Définition des propriétés (props) que ce composant accepte.
+  // Ces props sont utilisées pour passer des données au composant depuis l'extérieur.
+  props: ['url', 'text']
+}
+</script>
+
+<template>
+  <!-- Modèle du composant qui définit comment il est rendu -->
+  <a :href="url">{{ text }}</a>
+</template>
+
+<style>
+  /* Styles pour le composant NavLink, s'appliquent au lien (a) */
+  a {
+    text-decoration: none; /* Supprime la décoration de lien (soulignement) */
+  }
+</style>
